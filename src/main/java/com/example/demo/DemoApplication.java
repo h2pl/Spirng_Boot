ef06561e7d01d91060fa96e229fb55edf1770a6c
huangpenglong <huangpenglong@baidu.com>
2017-09-20 11:55:34 +0800

    add logback management

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,14 +2,9 @@ package com.example.demo;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@Configuration
-@ComponentScan(basePackages = "com.example.demo")
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = "com.example.demo")
 public class DemoApplication {
 
 	public static void main(String[] args) {
